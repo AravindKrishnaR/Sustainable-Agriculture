fd93aab0d929e9c687933833167abf4629b51c6c
Aravind Krishna <aravind2000krishna@gmail.com>
2021-05-19 00:42:01 +0530

    Update Flask code

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open('Model.pkl', 'rb'))
 
-@app.route('/', defaults={'page': 'Home.html'})
+@app.route('/', defaults = {'page': 'Home.html'})
 @app.route('/<page>')
 def html_lookup(page):
     return render_template('{}'.format(page))
 
 #To use the predict button in our web-app
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route('/YieldPrediction',methods = ['POST'])
+def YieldPrediction():
     
     if request.method == "POST":
        season = request.form.get("season")
